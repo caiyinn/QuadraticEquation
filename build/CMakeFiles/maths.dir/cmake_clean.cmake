08737a5f9bed049ad0052feaf28a0caa14556c24
cy <limcaiyin99@gmail.com>
2023-02-19 02:57:27 +0800

    modified to solve quadratic equation and added a cmakelist

---
+++ build/CMakeFiles/maths.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maths.dir/main.c.o"
+  "CMakeFiles/maths.dir/main.c.o.d"
+  "maths"
+  "maths.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/maths.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
